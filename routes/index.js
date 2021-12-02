635c57e3b4b406310833d1f6c2733cf3536e8a57
Utkarsh Upadhyay <utkarshup562@gmail.com>
2021-12-02 13:36:00 +0530

    Contact form and Thank you page

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,11 @@ router.get('/', function(req, res, next) {
 router.get('/about',function(req,res,next) {
   res.render('about',{ title: 'CodeMeet'});
 });
+router.get('/contact',function(req,res,next){
+  res.render('contact',{title:'CodeMeet'});
+})
+.post('/contact',function(req,res,next){
+  res.render('thanks',{title:'CodeMeet'});
+});
 
 module.exports = router;
