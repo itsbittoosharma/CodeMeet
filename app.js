f4494344e08726e0044710e195a4d49e3456e308
Utkarsh Upadhyay <utkarshup562@gmail.com>
2021-12-05 01:20:03 +0530

    Form Validation added successfully

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var expressValidator= require('express-validator'); 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var app = express();
-
+app.use(expressValidator());
+ 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var { body, validationResult} = require('express-validator');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -12,7 +12,22 @@ router.get('/contact',function(req,res,next){
   res.render('contact',{title:'CodeMeet'});
 })
 .post('/contact',function(req,res,next){
-  res.render('thanks',{title:'CodeMeet'});
+  
+  req.checkBody('name','Empty Name').notEmpty();
+  req.checkBody('email','Invalid email').isEmail();
+  req.checkBody('message','Empty message').notEmpty();
+  var errors=req.validationErrors();
+if(errors) {
+  res.render('contact',{
+    title:'CodeMeet',
+    name: req.body.name,
+    email:req.body.email,
+    message: req.body.message,
+    errorMessages: errors 
+  });
+}
+else
+  {res.render('thanks',{title:'CodeMeet'});}
 });
 
 module.exports = router;
